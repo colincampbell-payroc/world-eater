2f05811939677ed23c45f9406d20cf07644da1de
Andrew Clark <andrew.clark@payroc.com>
2023-10-27 15:48:32 -0400

    changes

--- bin/app.pl
+++ bin/app.pl
@@ -5,6 +5,8 @@ use warnings;
 
 use File::Basename;
 use FindBin;
+use Getopt::Long;
+use Sys::Hostname;
 
 use Data::Dumper;
 
@@ -13,11 +15,17 @@ use WorldEater::Config;
 
 my %config = %{ WorldEater::Config::get_all() };
 
+my $host = hostname;
+
+GetOptions( "host=s" => \$host )
+    or die("Error in command line arguments\n");
+
 my $dirname = dirname(__FILE__);
 
 mkdir "$dirname/../results";
 
-foreach my $directory ( @{ $config{directories} } ) {
+my $count = 0;
+foreach my $directory ( @{ $config{directories}{$host} } ) {
     my $last_directory = basename($directory);
 
     my $results_directory = "$dirname/../results/$last_directory";
@@ -26,19 +34,22 @@ foreach my $directory ( @{ $config{directories} } ) {
 
     mkdir $results_directory;
 
-    my $grep_command = $config{grep_command};
+    my $grep_command = $config{grep}{command};
+    my $grep_options = join( ' ', @{ $config{grep}{options} } );
 
     foreach my $grep ( @{ $config{greps} } ) {
         my $grep_filter = $grep->{filter};
         my $grep_type   = $grep->{type};
-        my $output      = `$grep_command '$grep_filter' $directory`;
+        my $output      = `$grep_command $grep_options '$grep_filter' $directory`;
 
-        open( my $results_fh, '>', "$results_directory/$grep_type.txt" ) or die $!;
+        open( my $results_fh, '>>', "$results_directory/$grep_type.txt" ) or die $!;
 
         print $results_fh $output;
 
         close($results_fh);
     }
+    $count++;
 }
-
+print "Host: $host\n";
+print "Directories checked: $count\n";
 print "Done!\n";
