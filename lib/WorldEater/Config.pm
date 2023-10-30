46d7a92009c93f38cc64dbca4f65d009fa6a80cd
Colin Campbell <colin.campbell@payroc.com>
2023-10-30 16:54:52 +0000

    Add api directories

--- lib/WorldEater/Config.pm
+++ lib/WorldEater/Config.pm
@@ -9,6 +9,7 @@ sub get_all {
             adminweb => [
                 qw{/home/batchupload /home/boarding /home/discoverportal /home/emailadmin /home/internalklmadmin /home/lantrans /home/mcf /home/monitor /home/pad /home/peoplestrust /home/reports /home/translookup /home/watcher /home/zuul }
             ],
+            api        => [qw {/home/boardingapi /home/paymentapi}],
             lt3        => [qw{ /home/cardinfo /home/lantrans }],
             lt3shared  => [qw{ /home/lantrans }],
             pad        => [qw{ /home/padapi }],
