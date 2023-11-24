b43e8f67a11573fb93ece6c89b712c5941e4894a
Colin Campbell <colin.campbell@payroc.com>
2023-11-24 11:08:02 +0000

    Parse all application config from spreadsheet

--- bin/parse_spreadsheet.pl
+++ bin/parse_spreadsheet.pl
@@ -21,6 +21,8 @@ my $outfile_path = "$FindBin::Bin/../data/SoftwareDiscovery.txt";
 
 open my $file_handle, '>', $outfile_path or die "Could not open file '$outfile_path' for writing: $!";
 
+print $file_handle "[";
+
 my @applications;
 foreach my $row ( 1 .. $applications_sheet->{MaxRow} ) {
     my $name = $applications_sheet->{Cells}[$row][3]->{Val};
@@ -32,12 +34,17 @@ foreach my $row ( 1 .. $applications_sheet->{MaxRow} ) {
 
     my $repo_url = $applications_sheet->{Cells}[$row][6]->{Val};
 
-    next if !defined($repo_url);
+    next if !$repo_url || $repo_url eq 'not in git';
 
     my ( $url, $branch, $sub_directory ) = WorldEater::RepoUrl::extract_parts($repo_url);
 
     my $server_name    = $applications_sheet->{Cells}[$row][0]->{Val};
     my $path_on_server = $applications_sheet->{Cells}[$row][4]->{Val};
+    my $ignore_list    = $applications_sheet->{Cells}[$row][7]->{Val};
+
+    my @parts = split( /\n/, $path_on_server );
+    $path_on_server = shift @parts;
+    my $extra_paths = join( ' ', @parts );
 
     my %application = (
         name               => $name,
@@ -45,9 +52,11 @@ foreach my $row ( 1 .. $applications_sheet->{MaxRow} ) {
         repo_sub_directory => defined $sub_directory ? $sub_directory : '',
         server_name        => $server_name,
         path_on_server     => $path_on_server,
+        ignore_list        => $ignore_list,
     );
 
-    $application{repo_branch} = $branch if defined $branch;
+    $application{repo_branch} = $branch      if defined $branch;
+    $application{extra_paths} = $extra_paths if $extra_paths;
 
     print $file_handle qq|
     {
@@ -56,14 +65,18 @@ foreach my $row ( 1 .. $applications_sheet->{MaxRow} ) {
         repo_sub_directory => '$application{repo_sub_directory}',
 |;
 
-    print $file_handle "        repo_branch        => '$application{repo_branch}'," if exists $application{repo_branch};
+    print $file_handle "        repo_branch        => '$application{repo_branch}',\n"     if exists $application{repo_branch};
+    print $file_handle "        extra_paths        => [qw{$application{extra_paths}}],\n" if exists $application{extra_paths};
 
     print $file_handle qq|        server_name        => '$application{server_name}',
         path_on_server     => '$application{path_on_server}',
+        ignore_list        => [qw{ $application{ignore_list} }]
     },
 |;
 
     push( @applications, \%application );
 }
+
+print $file_handle "]\n";
 
 close $file_handle;
