974b97a63ad2653e0b9bf1493a201435ff3cf5dd
Colin Campbell <colin.campbell@payroc.com>
2023-11-24 14:36:35 +0000

    Capture stderr to make identifying problems easier

--- bin/production_github_diff.pl
+++ bin/production_github_diff.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 
+use Capture::Tiny qw/capture/;
 use FindBin;
 
 use lib "$FindBin::Bin/../lib";
@@ -10,17 +11,21 @@ use WorldEater::Config;
 
 my $remote_user = $ARGV[0] || die "You must specify a remote user eg `./production_github_diff.pl ccampbell`";
 
-`rm -rf $FindBin::Bin/../code`;
-mkdir "$FindBin::Bin/../code";
-mkdir "$FindBin::Bin/../code/production";
-mkdir "$FindBin::Bin/../code/github";
+mkdir "$FindBin::Bin/../code"            unless -d "$FindBin::Bin/../code";
+mkdir "$FindBin::Bin/../code/production" unless -d "$FindBin::Bin/../code/production";
+mkdir "$FindBin::Bin/../code/github"     unless -d "$FindBin::Bin/../code/github";
 
 mkdir "$FindBin::Bin/../diffs" unless -d "$FindBin::Bin/../diffs";
 
 my %config = %{ WorldEater::Config::get_all() };
 
 foreach my $application ( @{ $config{applications} } ) {
-    `git clone $application->{repo_url} $FindBin::Bin/../code/github/$application->{name}`;
+    `rm -rf $FindBin::Bin/../code/production/$application->{name}`;
+    `rm -rf $FindBin::Bin/../code/github/$application->{name}`;
+
+    my $branch = exists( $application->{repo_branch} ) ? "-b $application->{repo_branch}" : '';
+
+    `git clone $branch $application->{repo_url} $FindBin::Bin/../code/github/$application->{name}`;
 
     my @ignore_list = ( @{ $application->{ignore_list} }, @{ $config{applications_ignore_list} } );
 
@@ -34,7 +39,23 @@ foreach my $application ( @{ $config{applications} } ) {
 # warn
 #     "rsync -vvv -r $exclude_flags -e \"ssh -t $remote_user\@bounce.caledoncard.com ssh -t\" $application->{server_name}:$application->{path_on_server}/ $FindBin::Bin/../code/production/$application->{name}";
 # next;
-    `rsync -r $exclude_flags -e "ssh -t $remote_user\@bounce.caledoncard.com ssh -t" $application->{server_name}:$application->{path_on_server}/ $FindBin::Bin/../code/production/$application->{name}`;
+
+    my ( $stdout, $stderr ) = capture {
+        system(
+            "rsync -r $exclude_flags -e \"ssh -t $remote_user\@bounce.caledoncard.com ssh -t\" $application->{server_name}:$application->{path_on_server}/ $FindBin::Bin/../code/production/$application->{name}"
+        );
+    };
+
+    $stderr =~ s/Pseudo-terminal will not be allocated because stdin is not a terminal.\r\n//g;
+
+    if ($stderr) {
+        my $outfile_path = "$FindBin::Bin/../diffs/$application->{name}.err";
+        open my $file_handle, '>', $outfile_path or die "Could not open file '$outfile_path' for writing: $!";
+
+        print $file_handle $stderr;
+
+        close $file_handle;
+    }
 
     if ( exists( $application->{extra_paths} ) ) {
         foreach my $path ( @{ $application->{extra_paths} } ) {
